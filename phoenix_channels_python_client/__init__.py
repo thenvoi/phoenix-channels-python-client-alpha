0f27c647b2aaab201bbfd6994cce30cc8f4a0f57
Dan Naiger <dannaiger@mac.home>
2025-09-04 17:36:28 +0300

    better seperation to support v2 protocol

--- phoenix_channels_python_client/__init__.py
+++ phoenix_channels_python_client/__init__.py
@@ -4,5 +4,13 @@ Phoenix Channels Python Client
 A Python client library for connecting to Phoenix Channels.
 """
 
+from phoenix_channels_python_client.client import PHXChannelsClient
+from phoenix_channels_python_client.protocol_handler import PHXProtocolHandler
+
 __version__ = "0.1.0"
 __author__ = "Phoenix Channels Python Client"
+
+__all__ = [
+    "PHXChannelsClient", 
+    "PHXProtocolHandler",
+]
