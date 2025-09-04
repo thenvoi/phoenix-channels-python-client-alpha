93fd950993ec6ef2012b344cc45c26ba89e263f0
Dan Naiger <dan.naiger@thenvoi.com>
2025-09-04 12:58:07 +0300

    clean not needed cursor stuff..

--- phoenix_channels_python_client/phx_messages.py
+++ phoenix_channels_python_client/phx_messages.py
@@ -1,44 +1,13 @@
-import asyncio
 from dataclasses import dataclass
 from enum import Enum, unique
 from functools import cached_property
-from typing import Any, NewType, Optional, Protocol, TYPE_CHECKING, Union
+from typing import Any, NewType, Optional, Union
 
-
-if TYPE_CHECKING:
-    from phoenix_channels_python_client.client import PHXChannelsClient
 
 
 Event = NewType('Event', str)
 ChannelEvent = Union['PHXEvent', Event]
 ChannelMessage = Union['PHXMessage', 'PHXEventMessage']
-
-
-class ExecutorHandler(Protocol):
-    """Protocol describing a handler that will be called using the provided executor pool"""
-
-    def __call__(self, __message: Union['PHXMessage', 'PHXEventMessage'], __client: 'PHXChannelsClient') -> None:
-        """
-        Args:
-            __message (Union[PHXMessage, PHXEventMessage]): The message the handler should process
-            __client (PHXChannelsClient): The client instance
-        """
-        ...  # pragma: no cover
-
-
-class CoroutineHandler(Protocol):
-    """Protocol describing a handler that will be run as a task in the event loop"""
-
-    async def __call__(self, __message: Union['PHXMessage', 'PHXEventMessage'], __client: 'PHXChannelsClient') -> None:
-        """
-        Args:
-            __message (Union[PHXMessage, PHXEventMessage]): The message the handler should process
-            __client (PHXChannelsClient): The client instance
-        """
-        ...  # pragma: no cover
-
-
-ChannelHandlerFunction = Union[ExecutorHandler, CoroutineHandler]
 
 
 @unique
