4309492bd790cfd9ed8ff53057aa9c97e9cff5dc
Dan Naiger <dannaiger@mac.home>
2025-09-05 18:20:52 +0300

    change url contruction

--- phoenix_channels_python_client/__init__.py
+++ phoenix_channels_python_client/__init__.py
@@ -4,7 +4,7 @@ Phoenix Channels Python Client
 A Python client library for connecting to Phoenix Channels.
 """
 
-from phoenix_channels_python_client.client import PHXChannelsClient
+from phoenix_channels_python_client.client import PHXChannelsClient, PhoenixChannelsProtocolVersion
 from phoenix_channels_python_client.protocol_handler import PHXProtocolHandler
 
 __version__ = "0.1.0"
@@ -13,4 +13,5 @@ __author__ = "Phoenix Channels Python Client"
 __all__ = [
     "PHXChannelsClient", 
     "PHXProtocolHandler",
+    "PhoenixChannelsProtocolVersion",
 ]
