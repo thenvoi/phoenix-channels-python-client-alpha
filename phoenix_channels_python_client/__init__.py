537c64c34aa076ddbef171654803ef236a192414
Dan Naiger <dan.naiger@thenvoi.com>
2025-09-05 21:46:28 +0300

    refactoring

--- phoenix_channels_python_client/__init__.py
+++ phoenix_channels_python_client/__init__.py
@@ -4,8 +4,8 @@ Phoenix Channels Python Client
 A Python client library for connecting to Phoenix Channels.
 """
 
-from phoenix_channels_python_client.client import PHXChannelsClient, PhoenixChannelsProtocolVersion
-from phoenix_channels_python_client.protocol_handler import PHXProtocolHandler
+from phoenix_channels_python_client.client import PHXChannelsClient
+from phoenix_channels_python_client.protocol_handler import PHXProtocolHandler, PhoenixChannelsProtocolVersion
 
 __version__ = "0.1.0"
 __author__ = "Phoenix Channels Python Client"
